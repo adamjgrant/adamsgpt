a3b6d5d3f489b950492fef16b2170e1f06bf6f9b
Adam Grant <hello@adamgrant.me>
2023-03-17 03:11:10 -0700

    Sidebar toggle done

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,9 @@
 import Component from "../../mozart.js";
+import sidebar from "../sidebar/sidebar.js";
 
 let navbar = new Component("navbar");
+
+let toggle = navbar.q(".toggle");
+toggle.addEventListener("click", sidebar.toggle);
 
 export default navbar;
--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -2,4 +2,12 @@ import Component from "../../mozart.js";
 
 let sidebar = new Component("sidebar");
 
+sidebar.toggle = function() {
+    let me = this.me;
+    if (me.classList.contains("show")) {
+        return me.classList.remove("show");
+    }
+    return me.classList.add("show");
+}
+
 export default sidebar;
