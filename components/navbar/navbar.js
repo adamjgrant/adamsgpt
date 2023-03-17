55cf3fae3badc308e3ae012f4411d5321f60d5cb
Adam Grant <hello@adamgrant.me>
2023-03-17 01:17:09 -0700

    Adding some basic styles to start

---
+++ components/navbar/navbar.js
@@ -0,0 +1,5 @@
+import Component from "../../mozart.js";
+
+let navbar = new Component("navbar");
+
+export default navbar;
--- new_component.sh
+++ new_component.sh
@@ -8,6 +8,6 @@ echo "import Component from \"../../mozart.js\";\n\nlet ${component_name} = new 
 echo "[data-component~='${component_name}'] {\n\n}" > components/$component_name/$component_name.css
 
 # Add component to array.
-sed "s/<\!\-\- Components \-\->/<\!\-\- Components \-\->\n\n    <\!\-\- ${component_name} \-\->\n    <script type=\"module\" src=\"\.\/components\/${component_name}\/${component_name}.js\"><\/script>\n    <link rel=\"stylesheet\" href=\".\/components\/${component_name}\/${component_name}.css\" \/>/" index.html | tee index.html > /dev/null
+#sed "s/<\!\-\- Components \-\->/<\!\-\- Components \-\->\n\n    <\!\-\- ${component_name} \-\->\n    <script type=\"module\" src=\"\.\/components\/${component_name}\/${component_name}.js\"><\/script>\n    <link rel=\"stylesheet\" href=\".\/components\/${component_name}\/${component_name}.css\" \/>/" index.html | tee index.html > /dev/null
 
 echo "${component_name} created."
